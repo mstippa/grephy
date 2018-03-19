3de5d2488d511de7509254dd1d83dd782f1d133d
Morgan Stippa <Morgan.Stippa1@marist.edu>
2018-03-19 11:58:09 -0400

    changed the component's properties to the state object

--- src/containers/regex.js
+++ src/containers/regex.js
@@ -10,29 +10,41 @@ export default class Regex extends Component {
 		this.state = { 
 			regex: '',
 			error: '',
+			indexInRegex: 0,
+			indexInTransitions: 0,
+			transitions: [],
+			curCharacter: '',
+			regexChar: {
+				'*': this.splat(),
+				'^': this.carrot(),
+				'|': this.alternator(),
+				'(': this.grouping(),
+				')': this.grouping(),
+				'$': this.eof(),
+				'.': this.period() 
+			}
 		};
 
-		this.indexInRegex = 0;
-		this.indexInTransitions = 0;
+		// this.indexInRegex = 0;
+		// this.indexInTransitions = 0;
 
-		// the transition function as a 3 dimensional array
-		// the index into the array represents a state	
-		this.transitions = [];
+		// // the transition function as a 3 dimensional array
+		// // the index into the array represents a state	
+		// this.transitions = [];
 
-		this.curCharacter;
-		this.regexChar = {
-			'*': this.splat,
-			'^': this.carrot,
-			'|': this.alternator,
-			'(': this.grouping,
-			')': this.grouping,
-			'$': this.eof,
-			'.': this.period 
-		};
+		// this.curCharacter;
+		// this.regexChar = {
+		// 	'*': this.splat,
+		// 	'^': this.carrot,
+		// 	'|': this.alternator,
+		// 	'(': this.grouping,
+		// 	')': this.grouping,
+		// 	'$': this.eof,
+		// 	'.': this.period 
+		// };
 
 		this.onFormSubmit = this.onFormSubmit.bind(this);
 		this.onInputChange = this.onInputChange.bind(this);
-
 	}
 
 	// checks if the the input is empty
@@ -40,20 +52,36 @@ export default class Regex extends Component {
 		return (this.state.regex === '');
 	}
 
- 
+	// recursive function
 	convertToNFA() {
+		// deciphering the regex
+		if (this.state.indexInRegex < this.state.regex.length) {
+			// changing the current character in the regex and passing in a callback function
+			this.setState({curCharacter: this.state.regex.charAt(this.state.indexInRegex)}, () => {
+				// seeing if the current character is a regexChar
+				if (this.state.curCharacter in this.state.regexChar) {					
+					this.state.regexChar[this.state.curCharacter]; // call the function associated with the regex character
+				} else {
+					this.character();
+				}
+				this.setState({indexInRegex: this.state.indexInRegex+1}); // increment the regex index
+				this.convertToNFA(); // keep calling the function until the regex is deciphered
+			});
+			
 
-		curCharacter = this.state.regex.charAt(indexInRegex);
+		// finished deciphering the regex
+		} else {
 
-		if (curCharacter in regexChar) {
-			indexInRegex++;
-			regexChar[curCharacter]; // call the function associated with the regex character
 		}
 
 	}
 
-	carrot() {
+	character() {
 
+	}
+
+	carrot() {
+		
 	}
 
 	alternator() {
@@ -86,14 +114,13 @@ export default class Regex extends Component {
 	// gets called when the form is submitted
 	onFormSubmit(event) {
 		event.preventDefault();
-
-		if(!this.validate()) {
+		if(this.validate() === false) {
 			this.setState({error: ''});
 			this.convertToNFA();
 		} else {
 			this.setState({error: 'Enter a regular expression'});
 		}
-		this.setState({regex: ''});
+		// this.setState({regex: ''});
 
 	}
 
