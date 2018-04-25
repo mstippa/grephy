98de08b46318a016ee4874cfea15e21efcdfe26a
Morgan Stippa <Morgan.Stippa1@marist.edu>
2018-04-25 19:54:06 -0400

    added another action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,24 @@
+// this file contains all the action creators
+// action creators happen when an event occurs
+// they return actions
+
 import fs from 'fs';
 
+// returns the plain text of a file
+// gets called from the File component
 export function fileUpload(file) {
 	return {
 		type: 'FILE_UPLOAD',
 		payload: file
 	}
 
+}
+
+// returns an array that represents the transition function
+// gets called from the Regex component
+export function transitionFunction(transitions) {
+	return {
+		type: 'TRANSITIONS',
+		payload: transitions
+	}
 }
