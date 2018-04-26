dc08bfefd9484bf39f30cdbe55f5af2af73f1917
Morgan Stippa <Morgan.Stippa1@marist.edu>
2018-04-26 16:35:37 -0400

    added more actions

--- src/reducers/reducer_output_box.js
+++ src/reducers/reducer_output_box.js
@@ -2,6 +2,12 @@ export default function (state = null, action) {
 	switch(action.type) {
 		case 'TRANSITIONS' :
 			return action.payload;
+		case 'CARROT' :
+			return action.payload;
+		case 'EOF' : 
+			return action.payload;
+		case 'ACCEPTING_STATE' :
+			return action.payload;		
 	}
 	return state;
 }
