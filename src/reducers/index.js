0071177cf1ddb5bf21864fa0247f161b3adb3372
Morgan Stippa <Morgan.Stippa1@marist.edu>
2018-04-26 16:36:06 -0400

    renamed the outputbox reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import InputBoxReducer from './reducer_input_box';
+import OutputBoxReducer from './reducer_output_box';
 
 const rootReducer = combineReducers({
-	input: InputBoxReducer
+	input: InputBoxReducer,
+	regexData: OutputBoxReducer
 });
 
 export default rootReducer;
