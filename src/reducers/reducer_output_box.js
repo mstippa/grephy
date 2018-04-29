c1016cc1b02fe5d2abb0c63a6813462853c1f940
Morgan Stippa <Morgan.Stippa1@marist.edu>
2018-04-29 15:14:18 -0400

    removed all the uneccessary actions

--- src/reducers/reducer_output_box.js
+++ src/reducers/reducer_output_box.js
@@ -1,13 +1,7 @@
 export default function (state = null, action) {
 	switch(action.type) {
-		case 'TRANSITIONS' :
-			return action.payload;
-		case 'CARROT' :
-			return action.payload;
-		case 'EOF' : 
-			return action.payload;
-		case 'ACCEPTING_STATE' :
-			return action.payload;		
+		case 'ACCEPTED_LINES' :
+			return action.payload;	
 	}
 	return state;
 }
