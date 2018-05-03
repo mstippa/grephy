5aaed0da4ec5695f9b6b6d51ac80131e9c6d89ba
Morgan Stippa <Morgan.Stippa1@marist.edu>
2018-05-03 11:02:15 -0400

    added the automata reducer to the root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import InputBoxReducer from './reducer_input_box';
 import OutputBoxReducer from './reducer_output_box';
+import AutomataReducer from './reducer_output_Automata';
 
 const rootReducer = combineReducers({
 	input: InputBoxReducer,
-	acceptedLines : OutputBoxReducer
+	acceptedLines : OutputBoxReducer,
+	automata: AutomataReducer
 });
 
 export default rootReducer;
