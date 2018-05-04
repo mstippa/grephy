f568c0a0cfac02ba1ca2fcc8d9edacf677597ec4
Morgan Stippa <Morgan.Stippa1@marist.edu>
2018-05-04 18:54:49 -0400

    using vis network to build directed graph

--- src/containers/NFA.js
+++ src/containers/NFA.js
@@ -5,32 +5,71 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import vis from 'vis';
 
-
 class NFA extends Component {
+
+	// creates a vis network using the NFA
+	renderNFA() {
+		var states = []; // initialize an array that will hold states
+		var transitions = []; // initialize an array for transitions
+		var NFA = this.props.NFA; 
+		var i = 0;		
+		var globalCounter = 0;		
+		var len = NFA.length;
+		for(; i < len; i++) { // loop through all the elements(states) in the NFA
+			states[i] = {"id": i};
+			states[i]["label"] = `${i}`;			
+			var state = NFA[i];
+			var transitionArray;
+			for (transitionArray in state) { // loop through all transitions for a state
+				var j = 0;
+				var numTransitions = state[transitionArray].length;
+				for(; j < numTransitions; j++) { // loop through all transitions for a state for a specific character
+					transitions[globalCounter] = {"from" : i };
+					transitions[globalCounter]["to"] = state[transitionArray][j];
+				}								
+				globalCounter++;
+			}
+		}
+		// add the accepting state
+		states[i] = {"id" : state[transitionArray][j-1]};
+		states[i]["label"] = `${i}`;	
+		
+	    var container = document.getElementById('nfa');
+
+	    // provide the data in the vis format
+	    var data = {
+	        nodes: states,
+	        edges: transitions
+	    };
+	    var options = {};
+	    // intitalize the vis network
+	    var network = new vis.Network(container, data, options);   
+	}
+
 	render() {
-		if (this.props.NFA === null) {
+		if (this.props.NFA === null) { // if NFA has not been created yet
 			return (
 				<div>
 					<h1>NFA</h1>
-					<div>I Like Poop</div>
+					<div id="nfa"></div>
 				</div>	
 			);
 		} else {
-			return (
+			this.renderNFA(); 
+		return (
 				<div>
 					<h1>NFA</h1>
-					<div>PP</div>
-				</div>
+					<div id="nfa"></div>
+				</div>	
 			);
 		}
-
 	}
 }
 
 function mapStateToProps(state) {
 	if (state.automata === null) return {NFA: null}
 	return {
-		NFA: state.automata.NFA
+		NFA: state.automata
 	}
 } 
 
