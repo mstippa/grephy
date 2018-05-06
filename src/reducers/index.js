af72ead82202c346f405ccbb096363f71298c0e5
Morgan Stippa <Morgan.Stippa1@marist.edu>
2018-05-05 21:10:08 -0400

    added the acceptedstatereducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import InputBoxReducer from './reducer_input_box';
 import OutputBoxReducer from './reducer_output_box';
 import AutomataReducer from './reducer_output_Automata';
+import AcceptedStateReducer from './reducer_accepted_state';
 
 const rootReducer = combineReducers({
 	input: InputBoxReducer,
 	acceptedLines : OutputBoxReducer,
-	automata: AutomataReducer
+	automata: AutomataReducer,
+	acceptedState: AcceptedStateReducer
 });
 
 export default rootReducer;
