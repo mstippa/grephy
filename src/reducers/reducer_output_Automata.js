9cbd929816dd47a46cb4aa360a45482b5229d9d7
Morgan Stippa <Morgan.Stippa1@marist.edu>
2018-05-07 12:02:30 -0400

    making sure everything is committed

--- src/reducers/reducer_output_Automata.js
+++
@@ -1,9 +0,0 @@
-export default function (state = null, action) {
-	switch (action.type) {
-		case 'DFA' :
-			return action.payload;
-		case 'NFA' :
-			return action.payload;
-	}
-	return state;
-}
